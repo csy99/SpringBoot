465480a66f7de3cf0ea46a7ec3ca71943cc72e7a
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2019-01-11 14:39:59 +0800

    reconstruct the repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# SpringBoot
+A demo website created using Spring Boot 2.0 framework 
+
+If you can read Chinese, you may find my blog useful. It is about the process of how I create a project and some keynotes. 
+https://blog.csdn.net/qq_40136685/article/details/86088596
---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Boot option. 
+ * @author CSY
+ *
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
